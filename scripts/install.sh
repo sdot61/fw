280cd3faea742bdbca3467a4e266040e1841ea6e
Van Schroeder <vanschroeder@mesmer.local>
2021-11-08 23:11:59 -0700

    updated installer command for python3.8 usage

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,5 @@
 sudo yum update
 sudo amazon-linux-extras enable python3.8
 sudo yum install python3.8 -y
-sudo yum install python3-pip -y
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8
+alias python=python3.8
 cd /app && pip install --user -r requirements.txt
