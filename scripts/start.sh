831a451e3c10c9a5250fc11263ffda424e7cbc91
Van Schroeder <vanschroeder@mesmer.local>
2021-11-09 06:49:59 -0700

    start command

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ sudo yum update -y
 cd /app
 pip3 install --user -r requirements.txt
 python3.8 -m pip install flask
-sudo python3.8 ./application.py &
+sudo python3.8 ./application.py >/dev/null 2>&1 &
