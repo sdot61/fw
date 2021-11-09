7ef64afd7d4c6766d0fe5a175d14c4864f810dcd
Van Schroeder <vanschroeder@mesmer.local>
2021-11-08 22:50:26 -0700

    updated installer command for python3-pip

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update
-#sudo yum remove python2.7 -y
+sudo yum remove python2.7 -y
 sudo yum install python37 -y
-sudo yum install python-pip -y
+sudo yum install python3-pip -y
 cd /app && pip install --user requirements.txt
