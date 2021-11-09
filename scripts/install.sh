ecbece79818a79a59c2e75fc9d29087d66f03da8
Van Schroeder <vanschroeder@mesmer.local>
2021-11-08 22:37:16 -0700

    updated installer command

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo yum update
-sudo yum remove python2.7 -y
+#sudo yum remove python2.7 -y
 sudo yum install python37 -y
 sudo yum install python-pip -y
-sudo yum install apache2 -y
-sudo yum install libapache2-mod-wsgi -y
 cd /app && pip install --user requirements.txt
