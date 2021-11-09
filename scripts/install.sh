9e596269da2ee83e512bef1a6a8cdfc5c0e024ef
Van Schroeder <vanschroeder@mesmer.local>
2021-11-08 22:30:23 -0700

    added remove for python2.7

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo yum update
+sudo yum remove python2.7 -y
 sudo yum install python37 -y
 sudo yum install python-pip -y
 sudo yum install apache2 -y
 sudo yum install libapache2-mod-wsgi -y
-cd /app && pip install -r requirements.txt
+cd /app && pip install --user requirements.txt
