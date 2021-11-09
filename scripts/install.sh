f33458adca39a002e15d9e1fa629fc3ff4738181
Van Schroeder <vanschroeder@mesmer.local>
2021-11-08 21:56:34 -0700

    added installer commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install python-pip
+sudo apt-get install python-flask
+sudo apt-get install apache2
+sudo apt-get install libapache2-mod-wsgi
 cd .. && pip install -r requirements.txt
